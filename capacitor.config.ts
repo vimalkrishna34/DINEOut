06c8bc005f494eb5c72bffaa2e6d28afdc0f6706
vimalkrishna34 <vima342005@gmail.com>
2024-10-03 01:08:43 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DineOutTheme',
+  webDir: 'www'
+};
+
+export default config;
